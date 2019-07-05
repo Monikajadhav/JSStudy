19db252a8f8d9ab1e17c0f978f349891e054fb04
Monika Jadhav <MonikaJ@WinjitTechnologiesPvtLtd.local>
2019-07-05 14:22:53 +0530

    initial commit

---
+++ variables/variables.js
@@ -0,0 +1,7 @@
+let a = 5
+
+console.log(a)
+
+const person = {  firstName: 'Tom',  lastName: 'Cruise',  actor: true,  age: 54} //made up}
+const {firstName: name, age} = person
+console.log(person.firstName);
