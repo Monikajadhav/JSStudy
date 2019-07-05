005df5965569ddbe3c21123e5fc58e747570d30c
Monika Jadhav <MonikaJ@WinjitTechnologiesPvtLtd.local>
2019-07-05 14:25:18 +0530

    yep its working

--- variables/variables.js
+++ variables/variables.js
@@ -5,3 +5,5 @@ console.log(a)
 const person = {  firstName: 'Tom',  lastName: 'Cruise',  actor: true,  age: 54} //made up}
 const {firstName: name, age} = person
 console.log(person.firstName);
+
+console.log("Code pushed")
